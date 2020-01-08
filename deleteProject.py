98687cef47d986a38a3e1bf7d1bebec76eb8ae1d
Aziz Fikri <45133910+afikrim@users.noreply.github.com>
2020-01-09 00:40:32 +0700

    Update deleteProject.py

--- deleteProject.py
+++ deleteProject.py
@@ -4,7 +4,25 @@ import getpass
 from termcolor import colored, cprint
 from github import Github
 
+
+options = [
+    ['-p|--project-name', 'Your new project name(required)'],
+    ['-l|--language',
+        'Your project language(required). Available language: PHP (php), JavaScript (js)'],
+    ['-t|--text-editor',
+        'Your command to open your favorite text editor. Ex: code, subl, etc.(required)'],
+    ['-c|--php-framework',
+        'Framework that you will use. All php framework that use composer are available. Usage: -c [composer dir] or --php-framework [composer dir], ex: -c laravel/laravel'],
+    ['--npm|--node-modules', 'Framework that you will use. All node modules are available. Usage: --npm [node modules] or --node-modules [node modules], ex --npm express socket.io'],
+    ['-g|--github', 'To confirm this project upload to github repos or not.'],
+]
+
+
 def main():
+    header()
+    if sys.argv[1] == 1:
+        error()
+        
     username = raw_input("Username for 'www.github.com': ")
     password = getpass.getpass("Password for '" + username + "'@github.com: ")
     user = Github(username, password)
@@ -19,6 +37,43 @@ def main():
         cprint("No repo found in github", "yellow")
 
     cprint("Process complete", "green")
+
+
+def header():
+    print('')
+    print('')
+    print colored(
+        '     ( A       ( U      ( U  ( T T T T T    ( O O O ', 'cyan')
+    print colored(
+        '    ( A A      ( U      ( U      ( T      ( O    ( O ', 'cyan')
+    print colored(
+        '   ( A ( A     ( U      ( U      ( T      ( O    ( O ', 'cyan')
+    print colored('  ( A   ( A    ( U      ( U      ( T      ( O    ( O     ',
+                  'cyan'), colored(' ( P P ( Y   Y', 'yellow')
+    print colored(' ( A A A A A    ( U     ( U      ( T      ( O    ( O     ',
+                  'cyan'), colored(' ( P  P ( Y Y', 'yellow')
+    print colored('( A       ( A     ( U U U        ( T        ( O O O  ( O)',
+                  'cyan'), colored(' ( P P   ( Y', 'yellow')
+    print colored('                                                         ',
+                  'cyan'), colored(' ( P    ( Y', 'yellow')
+    print colored('                                                         ',
+                  'cyan'), colored(' ( P   ( Y', 'yellow')
+    print('')
+    print('')
+
+
+def error():
+    cprint("Usage :", "yellow")
+    cprint("  create [option] [argument]\n", 'white')
+    cprint("  delete [project-name]\n", 'white')
+    cprint("Ex :", "yellow")
+    cprint("  create -p demo -l js -t code --npm --save express socket.io\n", 'white')
+    cprint("  delete demo\n", 'white')
+    cprint("Options :", "yellow")
+    global options
+    for option in options:
+        print colored('  %-20s' % option[0],
+                      'green'), colored(option[1], 'white')
 
 if __name__ == "__main__":
     main()
